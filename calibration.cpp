419a78543ed6edff57ffc2e0c647471a37e8de19
= <nw265@cornell.edu>
2017-03-04 15:51:33 -0500

    added calibration's destructor

--- calibration.cpp
+++ calibration.cpp
@@ -4,6 +4,8 @@ Calibration::Calibration() {
 	//TODO to be implemented
 }
 
+Calibration::~Calibration() {}
+
 void Calibration::undistortImages(cv::Mat& imgLeft, cv::Mat& imgRight, cv::Mat& imgLeftDest, cv::Mat& imgRightDest) {
 	//TODO to be implemented
 }
