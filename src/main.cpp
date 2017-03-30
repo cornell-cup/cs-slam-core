63f0523b24c71f7acc0dcd2b6bb6024e68265247
Daniel Weber <dantech88@yahoo.com>
2017-03-30 16:53:58 -0400

    added mouse callback singleton and visualizer base

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,7 @@
 
 #include "disparitypipeline.h"
 #include "meshgenerator.h"
-
-std::function<int(int, int)> getDispValue;
-
-void mouseCallBackFunc(int event, int x, int y, int flags, void* userdata) {
-     if  (event == cv::EVENT_LBUTTONDOWN ){
-          std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
-					std::cout << "Disparity value: " << getDispValue(x,y) << std::endl;
-     }
-     else if  (event == cv::EVENT_RBUTTONDOWN){
-          std::cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
-     }
-     else if  (event == cv::EVENT_MBUTTONDOWN){
-          std::cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
-     }
-     else if (event == cv::EVENT_MOUSEMOVE){
-          std::cout << "Mouse move over the window - position (" << x << ", " << y << ")" << std::endl;
-     }
-}
-
-
+#include "mousehandler.h"
 
 int main() {
 	std::cout << "starting program" << std::endl;
@@ -35,12 +16,7 @@ int main() {
 
 	DisparityPipeline pipeline = DisparityPipeline(leftCamera, rightCamera);
 
-	getDispValue = [&pipeline](int x, int y) -> int {
-		return pipeline.getDepthAt(y,x);
-	};
-
-  // set the mouse callback
-  pipeline.setDisparityMouseCallback(mouseCallBackFunc);
+	MouseHandler::initialize(&pipeline);
 
 	MeshGenerator meshGenerator = MeshGenerator();
 
---
+++ src/mousehandler.cpp
@@ -0,0 +1,33 @@
+#include "mousehandler.h"
+
+MouseHandler* MouseHandler::_instance;
+
+MouseHandler* MouseHandler::initialize(DisparityPipeline* pipeline) {
+  if(!_instance) {
+    _instance = new MouseHandler(pipeline);
+  }
+  return _instance;
+}
+
+MouseHandler::~MouseHandler(){}
+
+MouseHandler::MouseHandler(DisparityPipeline* pipeline) {
+  _pipe = pipeline;
+  _pipe->setDisparityMouseCallback(_mouseEventCallback);
+}
+
+void MouseHandler::_mouseEventCallback(int event, int x, int y, int flags, void* userdata) {
+     if  (event == cv::EVENT_LBUTTONDOWN ){
+          std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
+					std::cout << "Disparity value: " << _instance->_pipe->getDepthAt(y,x) << std::endl;
+     }
+     else if  (event == cv::EVENT_RBUTTONDOWN){
+          std::cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
+     }
+     else if  (event == cv::EVENT_MBUTTONDOWN){
+          std::cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
+     }
+     else if (event == cv::EVENT_MOUSEMOVE){
+          std::cout << "Mouse move over the window - position (" << x << ", " << y << ")" << std::endl;
+     }
+}
